29db34e2e1785dae81feef5055ea3d960d07ad9c
Abu Raihan <aburaihancse066@gmail.com>
2022-12-17 04:33:47 +0600

    Add Constant

---
+++ src/Services/Action.js
@@ -0,0 +1,7 @@
+import { REQUESR_DATA, GET_DATA, ERROR_DATA } from './Constant';
+
+export const requestData = () => {
+    return {
+        type: REQUESR_DATA
+    }
+}
---
+++ src/Services/Constant.js
@@ -0,0 +1,3 @@
+export const REQUESR_DATA = "REQUESR_DATA"
+export const GET_DATA = "GET_DATA"
+export const ERROR_DATA = "ERROR_DATA"
