943abace8636ea886b63cdb7ce66cbcb35ea2975
Abu Raihan <aburaihancse066@gmail.com>
2022-12-17 04:41:18 +0600

    Add Reducer

---
+++ src/Services/Reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    isLoading: false,
+    data: [],
+    error: null
+
+}
+
+
+const reducer = (state = initialState, action){
+    switch (action.type) {
+        case "REQUEST_DATA":
+            return {
+                ...state,
+                isLoading: true
+            }
+        case "GET_DATA":
+            return {
+                ...state,
+                data: action.payload,
+                isLoading: false
+            }
+        case "ERROR_DATA":
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
