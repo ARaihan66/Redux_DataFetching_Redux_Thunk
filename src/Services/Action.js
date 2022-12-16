37aa30a4d27975b4aab7327c3344bc209f6dc994
Abu Raihan <aburaihancse066@gmail.com>
2022-12-17 04:36:40 +0600

    Add Action

--- src/Services/Action.js
+++ src/Services/Action.js
@@ -1,7 +1,21 @@
-import { REQUESR_DATA, GET_DATA, ERROR_DATA } from './Constant';
+import { REQUEST_DATA, GET_DATA, ERROR_DATA } from './Constant';
 
 export const requestData = () => {
     return {
-        type: REQUESR_DATA
+        type: REQUEST_DATA
+    }
+}
+
+export const getData = (data) => {
+    return {
+        type: GET_DATA,
+        payload: data
+    }
+}
+
+export const errorData = (err) => {
+    return {
+        type: ERROR_DATA,
+        payload: err
     }
 }
--- src/Services/Constant.js
+++ src/Services/Constant.js
@@ -1,3 +1,3 @@
-export const REQUESR_DATA = "REQUESR_DATA"
+export const REQUEST_DATA = "REQUEST_DATA"
 export const GET_DATA = "GET_DATA"
 export const ERROR_DATA = "ERROR_DATA"
